ede14b3168ce4d49af4b9ca66477b614faaaf3d3
Efosa Osasu <efosaosasu20@gmail.com>
2024-08-12 18:03:21 +0100

    Production phase

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -39,8 +39,8 @@ const Item = ({ item, width }) => {
           alt={item.name}
           width="280px"
           height="400px"
-          // src={`https://unique-wears-server-v1.vercel.app/${url}`}
-          src={`http://localhost:1337${url}`}
+          src={`https://unique-wears-server.vercel.app/${url}`}
+          // src={`http://localhost:1337${url}`}
           onClick={() => navigate(`/item/${item.id}`)}
           style={{ cursor: "pointer" }}
         />
